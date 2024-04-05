cb32aacc5512f2f6e9bc82a5b5476f46803c46b8
Billy Murphy <billy@prodatakey.com>
2024-04-05 14:44:49 -0600

    fixit

--- next_app/src/services/genericHttpManyRequests/index.ts
+++ next_app/src/services/genericHttpManyRequests/index.ts
@@ -2,63 +2,63 @@ import { delay, dynamicUrlGenerator } from "@/utilities/serviceUtilities";
 import * as http from "http";
 
 /**
- * This service is here in an attempt to reporduce a bug in a production next JS application without using node's Native fetch api
+ * This service is here in an attempt triangluate the issue with out going requests
  */
 
 interface FetchOptions {
-  method: string;
-  headers?: http.OutgoingHttpHeaders;
-  body?: any;
+  method: string
+  headers?: http.OutgoingHttpHeaders
+  body?: any
 }
 
 function customFetch(url: string, options: FetchOptions): Promise<string> {
   return new Promise<string>((resolve, reject) => {
     const req = http.request(url, options, (res) => {
-      let responseData = "";
+      let responseData = ""
 
-      res.setEncoding("utf8");
+      res.setEncoding("utf8")
 
       res.on("data", (chunk) => {
-        responseData += chunk;
-      });
+        responseData += chunk
+      })
 
       res.on("end", () => {
-        resolve(responseData);
-      });
-    });
+        resolve(responseData)
+      })
+    })
 
     req.on("error", (error) => {
-      reject(error);
-    });
+      reject(error)
+    })
 
     if (options.body) {
-      req.write(options.body);
+      req.write(options.body)
     }
 
-    req.end();
-  });
+    req.end()
+  })
 }
 
 async function sendRequest() {
   try {
-    const randomUrlToekn = dynamicUrlGenerator(1000);
-    const url = "http://localhost:3001/" + randomUrlToekn;
+    const randomUrlToken = dynamicUrlGenerator(1000)
+    const url = "http://localhost:3001/" + randomUrlToken
     const options: FetchOptions = {
       method: "GET",
     };
-    const responseData = await customFetch(url, options);
-    console.log("Response:", responseData);
+    const responseData = await customFetch(url, options)
+    console.log("Response:", responseData)
   } catch (error) {
-    console.error("sendRequest error:", error);
+    console.error("sendRequest error:", error)
   }
 }
 
 export async function start() {
-  let i = 0;
-  console.log("Starting genericHttpManyRequests background service");
+  let i = 0
+  console.log("Starting genericHttpManyRequests background service")
   while (i < 10000) {
-    await sendRequest();
-    await delay(5);
-    i++;
+    await sendRequest()
+    await delay(5)
+    i++
   }
 }
